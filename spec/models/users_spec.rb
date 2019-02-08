299af36a3e4dc37b4f701da15cdc144f86e4fa48
muneichi2 <muneichi.x07@gmail.com>
2019-02-08 16:50:28 +0900

    RSpec 商品削除
    #その他修正あり

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,51 +1,52 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe User do
-  describe '#create' do
+RSpec.describe User, type: :model do
 
-    it "is valid with a nickname, email, password, password_confirmation" do
+  describe "#create" do
+
+    it "nickname, email, password, password_confirmationが存在すれば登録できる" do
       user = build(:user)
       expect(user).to be_valid
     end
 
-    it "is invalid without a nickname" do
+    it "nicknameが空の場合は登録できない" do
       user = build(:user, nickname: nil)
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
 
-    it "is invalid without a email" do
+    it "emailが空の場合は登録できない" do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
-    it "is invalid without a password" do
+    it "passwordが空の場合は登録できない" do
       user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("can't be blank")
     end
 
-     it "is invalid without a password_confirmation although with a password" do
+     it "password_confirmationが空の場合は登録できない" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
 
-    it "is invalid with a nickname that has more than 20 characters " do
+    it "nicknameが21文字以上の場合は登録できない" do
       user = build(:user, nickname: "aaaaaaaaaaaaaaaaaaaaa")
       user.valid?
       expect(user.errors[:nickname][0]).to include("is too long")
     end
 
-    it "is invalid with a duplicate email address" do
+    it "既に登録されているemailが存在する場合は登録できない" do
       user = create(:user)
       another_user = build(:user)
       another_user.valid?
       expect(another_user.errors[:email]).to include("has already been taken")
     end
 
-    it "is invalid with a password that has less than 5 characters " do
+    it "passwordが5文字以下の場合は登録できない" do
       user = build(:user, password: "00000", password_confirmation: "00000")
       user.valid?
       expect(user.errors[:password][0]).to include("is too short")
